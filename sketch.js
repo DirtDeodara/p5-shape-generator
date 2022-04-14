d15f55180932cb0bccdced670b108190c21ffb41
Dirt Deodara <dirtdeodara@gmail.com>
2022-04-14 09:34:34 -0700

    First work within sketch

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(500, 500, SVG)
+  noLoop()
+  angleMode(DEGREES)
+  rectMode(CENTER)
+}
+
+function draw() {
+  background('teal')
+  fill(0)
+  push()
+    translate(width/2, height/2)
+    rotate(45)
+    rect(0, 0, 25, 25)
+  pop()
+  fill('red')
+  rect(0, 0, 25, 25)
+}
